fd8e44c31288afdaa1211bf2e291b5c2051d8289
Bat-Nic <36857720+JeuxDeBio@users.noreply.github.com>
2018-03-12 13:03:32 -0400

    Update Monde_Drag_Drop.java

--- src/ca/qc/bdeb/vue/dragDrop/Monde_Drag_Drop.java
+++ src/ca/qc/bdeb/vue/dragDrop/Monde_Drag_Drop.java
@@ -8,7 +8,7 @@ package ca.qc.bdeb.vue.dragDrop;
 import ca.qc.bdeb.controleur.Controleur;
 import ca.qc.bdeb.modele.Jeu;
 import ca.qc.bdeb.modele.Modele;
-import ca.qc.bdeb.vue.principale.Fenetre_jeu;
+import ca.qc.bdeb.vue.principale.FenetreJeu;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -24,19 +24,19 @@ import javax.swing.JOptionPane;
  *
  * @author 1649904
  */
-public class Monde_Drag_Drop extends JComponent {
+public class MondeDragDrop extends JComponent {
 
     private Modele modele;
     private Controleur controleur;
 
     private JLabel lblTimer;
     private int timer = 0, compteur = 0;
-    private Fenetre_jeu fenetre;
+    private FenetreJeu fenetre;
 
     private Image image;
 
-    private ArrayList<Ronde_question> listeQuestions = new ArrayList<>();
-    private ArrayList<Boite_reponse> listeReponses = new ArrayList<>();
+    private ArrayList<RondeQuestion> listeQuestions = new ArrayList<>();
+    private ArrayList<BoiteReponse> listeReponses = new ArrayList<>();
 
     private final int largeur = 800, hauteur = 600;
 
@@ -44,7 +44,7 @@ public class Monde_Drag_Drop extends JComponent {
         @Override
         public void run() {
             super.run(); //To change body of generated methods, choose Tools | Templates.
-            while (true) {
+            while (!finJeu) {
 
                 bougerQuestions();
                 //verification();
@@ -60,8 +60,8 @@ public class Monde_Drag_Drop extends JComponent {
         }
     };
 
-    public Monde_Drag_Drop(JLabel lblTimer, Fenetre_jeu fenetre, Controleur controleur, Modele modele) {
-        this.setPreferredSize(new Dimension(800, 600));
+    public MondeDragDrop(JLabel lblTimer, FenetreJeu fenetre, Controleur controleur, Modele modele) {
+        this.setPreferredSize(new Dimension(largeur, hauteur));
         this.setLayout(null);
 
         this.modele = modele;
@@ -84,7 +84,7 @@ public class Monde_Drag_Drop extends JComponent {
 
         for (int i = 0; i < coordonnees.length; i++) {
             if (texte.size() == coordonnees.length) {
-                Ronde_question question = new Ronde_question(texte.get(i), ((hauteur / coordonnees.length) * i) + 20);
+                RondeQuestion question = new RondeQuestion(texte.get(i), ((hauteur / coordonnees.length) * i) + 20);
                 question.setInitialX(largeur - question.getWidth() - 20);
                 this.add(question);
                 listeQuestions.add(question);
@@ -94,7 +94,7 @@ public class Monde_Drag_Drop extends JComponent {
 
         for (int i = 0; i < coordonnees.length; i++) {
             if (texte.size() == coordonnees.length) {
-                Boite_reponse reponse = new Boite_reponse(texte.get(i));
+                BoiteReponse reponse = new BoiteReponse(texte.get(i));
                 this.add(reponse);
                 listeReponses.add(reponse);
                 reponse.setLocation(coordonnees[i][0], coordonnees[i][1]);
@@ -104,8 +104,8 @@ public class Monde_Drag_Drop extends JComponent {
     }
 
     private void creerEvenements() {
-        for (Ronde_question question : listeQuestions) {
-            for (Boite_reponse reponse : listeReponses) {
+        for (RondeQuestion question : listeQuestions) {
+            for (BoiteReponse reponse : listeReponses) {
                 question.addMouseListener(new MouseAdapter() {
                     @Override
                     public void mousePressed(MouseEvent me) {
@@ -154,21 +154,22 @@ public class Monde_Drag_Drop extends JComponent {
     }
 
     private void bougerQuestions() {
-        try {
+      
             for (Ronde_question question : listeQuestions) {
+                try{
                 if (question.hold()) {
                     if (question.getX() < 5) {
                         question.holdFalse();
-                        question.setLocation(10, (int) this.getMousePosition().getY() - 25);
+                        question.setLocation(question.getInitialX(), question.getInitialY());
                     } else if (question.getX() + question.getWidth() > largeur - 5) {
                         question.holdFalse();
-                        question.setLocation(largeur - question.getWidth() - 10, (int) this.getMousePosition().getY() - 25);
+                        question.setLocation(question.getInitialX(), question.getInitialY());
                     } else if (question.getY() < 5) {
                         question.holdFalse();
-                        question.setLocation((int) this.getMousePosition().getX() - 10, 10);
+                        question.setLocation(question.getInitialX(), question.getInitialY());
                     } else if (question.getY() + question.getHeight() > hauteur - 5) {
                         question.holdFalse();
-                        question.setLocation((int) this.getMousePosition().getX() - 10, hauteur - question.getHeight() - 10);
+                        question.setLocation(question.getInitialX(), question.getInitialY());
                     } else {
                         question.setLocation((int) this.getMousePosition().getX() - 10, (int) this.getMousePosition().getY() - 25);
                     }
@@ -179,8 +180,8 @@ public class Monde_Drag_Drop extends JComponent {
     }
 
     private void verification() {
-        for (Ronde_question question : listeQuestions) {
-            for (Boite_reponse reponse : listeReponses) {
+        for (RondeQuestion question : listeQuestions) {
+            for (BoiteReponse reponse : listeReponses) {
                 if (reponse.getBounds().intersects(question.getBounds()) && !reponse.estOccupe()) {
                     question.holdFalse();
                     reponse.occupeTrue(question);
@@ -193,12 +194,12 @@ public class Monde_Drag_Drop extends JComponent {
     }
 
     public void validationPoints() {
-        ArrayList<Ronde_question> listeQuestionsPasRepondus = new ArrayList<>();
+        ArrayList<RondeQuestion> listeQuestionsPasRepondus = new ArrayList<>();
         String motsClesManquants = "";
         String motsClesFausses = "";
         int nombreErreurs = 0;
 
-        for (Ronde_question question : listeQuestions) {
+        for (RondeQuestion question : listeQuestions) {
             if (!question.occupe()) {
                 listeQuestionsPasRepondus.add(question);
                 motsClesManquants += " - " + question.getBoite().getTexte() + "\n";
@@ -208,7 +209,7 @@ public class Monde_Drag_Drop extends JComponent {
         if (!listeQuestionsPasRepondus.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Vous n'avez pas place toutes les mots cles\nVoici ceux qui manquent: \n\n" + motsClesManquants, "Jeu Incomplet", JOptionPane.WARNING_MESSAGE);
         } else {
-            for (Boite_reponse reponse : listeReponses) {
+            for (BoiteReponse reponse : listeReponses) {
                 if (!reponse.bonneReponse()) {
                     motsClesFausses += " - " + reponse.getQuestion().getBoite().getTexte() + "\n";
                     nombreErreurs++;
